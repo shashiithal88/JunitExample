96207e4f82ee96acaf6c942bba8b6c2c4a41ccb1
shashiithal88 <33441279+shashiithal88@users.noreply.github.com>
2018-03-27 11:48:46 +0530

    Update TestHelloWorld.java

--- src/test/java/com/mslc/TestHelloWorld.java
+++ src/test/java/com/mslc/TestHelloWorld.java
@@ -26,7 +26,7 @@ public class TestHelloWorld {
 	   public void testHelloWorld() 
 	   {
 	      h.setName("World");
-	      assertEquals(h.getName(),"WOrld");
-	      assertEquals(h.getMessage(),"HeEllo World!");
+	      assertEquals(h.getName(),"World");
+	      assertEquals(h.getMessage(),"Hello World!");
 	   }
 }
